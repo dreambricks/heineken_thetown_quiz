0aee6e1b39540f905bf047650d91f6b8d642d22a
Julio Balbino <julio4110@hotmail.com>
2023-08-01 15:43:53 -0300

    inicial files

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+
+    public List<AudioClip> audios = new List<AudioClip>();
+    public List<string> alternatives = new List<string>();
+    public string answer;
+
+}
